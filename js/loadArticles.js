4ab74854fd065fec01a721fbc613c53c3c84f926
JelsonMatheus <58706567+JelsonMatheus@users.noreply.github.com>
2020-12-14 13:48:15 -0300

    Create loadArticles.js

---
+++ js/loadArticles.js
@@ -0,0 +1,32 @@
+(function loadArticles() {
+    fetch("../artigos.json")
+    .then(response => response.json())
+    .then(data => showArticles(data))
+    .catch(console.error);
+})();
+
+function showArticles(artigosArray) {
+    const divCards = document.getElementById('div-cards');
+    
+    const card = document.getElementById('card');
+    const category = document.getElementById('category');
+    const img = document.getElementById('img');
+    const title = document.getElementById('title');
+    const text = document.getElementById('text');
+    const data = document.getElementById('data');
+    const link = document.getElementById('link');
+    
+    divCards.innerHTML = "";
+    card.style.display = "block";
+    
+    artigosArray.reverse().forEach(artigo => {
+        category.innerText = artigo.categoria;
+        img.src = artigo.img;
+        title.innerText = artigo.titulo;
+        text.innerHTML = artigo.texto;
+        data.innerText = artigo.data;
+        link.href = artigo.link;
+        
+        divCards.innerHTML += card.outerHTML;
+    });
+}
